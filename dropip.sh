12bfc76f05d524f834a9f62f63c850a968f6cdff
extrick-jp <y.oota@extrick.jp>
2023-09-23 12:22:03 +0900

    Delete

--- dropip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /usr/local/bin/dropip
-/bin/python3 ./dropip.py -s apache -l /var/log/httpd/access_log || {
-    cat exec.log
-}
-cat ./foreign.ip ./deny.conf > ./htaccess
-cp ./htaccess /var/www/SITE_NAME/html/.htaccess
